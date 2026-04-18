55da017aa0528910c4d1b1996506336c460b1a33
Kevin <phamhoangphuc613p@gmail.com>
2026-04-19 00:23:47 +0700

    refactor(sitemap): streamline static page definitions and enhance error handling

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,43 +2,27 @@ import { ArticleWithTags } from '@/public/lib/types';
 import { getArticles } from '@/services/articles.user.service';
 import { MetadataRoute } from 'next'
 
+// Buộc route này luôn render động
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = 'https://www.devstackpro.cloud'
 
-    // Các trang tĩnh
     const staticPages: MetadataRoute.Sitemap = [
-        {
-            url: baseUrl,
-            lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
-        },
-        {
-            url: `${baseUrl}/articles`,
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
-        {
-            url: `${baseUrl}/contact`,
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
+        { url: baseUrl, lastModified: new Date(), changeFrequency: 'yearly', priority: 1 },
+        { url: `${baseUrl}/articles`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
+        { url: `${baseUrl}/contact`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
     ]
 
-    // Lấy các bài viết động từ API/database
     try {
-
         const articles = await getArticles();
         if (!articles || articles.length === 0) {
             return staticPages;
         }
+
         const postPages: MetadataRoute.Sitemap = articles.map((article: ArticleWithTags) => ({
             url: `${baseUrl}/articles/${article.slug}`,
-            lastModified: article.updated_at
-                ? new Date(article.updated_at)
-                : new Date(),
+            lastModified: article.updated_at ? new Date(article.updated_at) : new Date(),
             changeFrequency: 'weekly' as const,
             priority: 0.7,
         }))
