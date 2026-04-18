e2a7c3ca0be7abdbac1975d2fe286083bf08b200
Kevin <phamhoangphuc613p@gmail.com>
2026-04-19 00:02:31 +0700

    refactor(supabase): simplify createClient by removing external cookieStore parameter

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,28 +1,30 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
+// Không cần truyền cookieStore từ ngoài vào nữa
+export const createClient = async () => {
+  const cookieStore = await cookies();
   return createServerClient(
-    supabaseUrl!,
-    supabaseKey!,
+    supabaseUrl,
+    supabaseKey,
     {
       cookies: {
         getAll() {
-          return cookieStore.getAll()
+          return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
           } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // Ignore nếu gọi từ Server Component
           }
         },
       },
-    },
+    }
   );
 };
